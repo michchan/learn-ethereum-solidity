1516005a2de025c99597d62021c0f25349a9f69f
Michael L.S. Chan <michael.ls.chan@axa.com.hk>
2021-03-16 12:06:04 +0800

    feat: Lottery contract: Add "random" function

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -5,17 +5,30 @@ contract Lottery {
     address[] public players;
     
     function Lottery() public {
-        // "msg" is a global variable
+        // @global: "msg" is a global variable
         manager = msg.sender;
     }
     
     function enter() public payable {
-        // "require" is a global variable
+        // @global: "require" is a global variable
+        //
         // Make sure some requirement is satisfied before
         // the following commands to be executed
+        //
         // Here, we make sure the sneder has at least a certain amount of Ether
         // in order to enter into the contract
         require(msg.value > .01 ether);
         players.push(msg.sender);
+    }
+    
+    // Return an "unsigned int",
+    // "uint" here by default means "uint256"
+    function random() private view returns (uint) {
+        // SHA256
+        //  @global: "block" and "now" are global variables
+        // 
+        // The return value of "keccak256" is a hash.
+        // We convert it to unit by casting it.
+        return uint(keccak256(block.difficulty, now, players));
     }
 }
