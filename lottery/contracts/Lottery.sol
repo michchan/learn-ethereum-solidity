d0dc25edf9f9f512a7d6291b966dc22301cc7bed
Michael L.S. Chan <michael.ls.chan@axa.com.hk>
2021-03-16 11:55:44 +0800

    feat: enter function of Lottery

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -2,9 +2,20 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
+    address[] public players;
     
     function Lottery() public {
         // "msg" is a global variable
         manager = msg.sender;
+    }
+    
+    function enter() public payable {
+        // "require" is a global variable
+        // Make sure some requirement is satisfied before
+        // the following commands to be executed
+        // Here, we make sure the sneder has at least a certain amount of Ether
+        // in order to enter into the contract
+        require(msg.value > .01 ether);
+        players.push(msg.sender);
     }
 }
